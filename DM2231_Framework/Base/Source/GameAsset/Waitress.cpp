874861f58451099ed725c36cf83080a86945c9d5
Setsuna <sweeseng123@gmail.com>
2015-11-27 10:25:44 +0800

    Did Waitress FSM

--- DM2231_Framework/Base/Source/GameAsset/Waitress.cpp
+++ DM2231_Framework/Base/Source/GameAsset/Waitress.cpp
@@ -4,7 +4,8 @@
 
 Waitress::Waitress()
 {
-	type = STATIC_OBJ;
+	currentState = s_Idle;
+	previousState = s_Idle;
 }
 
 
@@ -20,4 +21,43 @@ void Waitress::setMesh(Mesh* mesh)
 Mesh* Waitress::getMesh()
 {
 	return mesh;
+}
+
+void Waitress::update(const double & dt)
+{
+	if (currentState == previousState == s_Idle)
+	{
+		//Check whether there is customer
+		//if(customer == true && customer == true)
+		{
+			currentState = s_Usher;
+		}
+	}
+
+	switch (currentState)
+	{
+	case(s_Usher) :
+		//Usher customer to their seat
+		break;
+
+	case(s_TakeOrder):
+		//Take order from customer
+		break;
+		
+	case(s_PlaceOrder) :
+		//Place order with chef
+		break;
+
+	case(s_TakeFood) :
+		//Take order from chef
+		break;
+
+	case(s_DeliverFood) :
+		//Delivery food to customer
+		break;
+
+	case(s_CleanTable) :
+		//Clean table after customer is done
+		break;
+	}
 }
--- DM2231_Framework/Base/Source/GameAsset/Waitress.h
+++ DM2231_Framework/Base/Source/GameAsset/Waitress.h
@@ -3,14 +3,28 @@
 
 class Waitress : public CGameObject
 {
+	enum STATES
+	{
+		s_Idle,
+		s_Usher,
+		s_TakeOrder,
+		s_PlaceOrder,
+		s_TakeFood,
+		s_DeliverFood,
+		s_CleanTable
+	};
+
 public:
 	Waitress();
 	~Waitress();
 
 	void setMesh(Mesh* mesh);
+	void update(const double & dt);
 
 	Mesh* getMesh();
 private:
 	Mesh* mesh;
+	STATES currentState;
+	STATES previousState;
 };
 
